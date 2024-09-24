c7dd2d41fdb49a9159782bb864b8d45de5e5635c
Pwongchaiya <Pwongchaiya1@gmail.com>
2024-09-24 14:13:15 -0700

    Ticket Controller E2E

---
+++ TicketMGT.Core.Api.Tests.Acceptance/Apis/Tickets/TicketsApiTests.Logic.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using TicketMGT.Core.Api.Models.Foundations.Tickets;
+
+namespace TicketMGT.Core.Api.Tests.Acceptance.Apis.Tickets
+{
+    public partial class TicketsApiTests
+    {
+        /*[Fact]
+        private async Task ShouldPostTicketAsync()
+        {
+            // given
+            Ticket randomTicket = CreateRandomTicket();
+            Ticket inputTicket = randomTicket;
+            Ticket expectedTicket = inputTicket;
+
+            // when
+            await this.ticketApiBroker
+                .PostTicketAsync(inputTicket);
+
+            Ticket actualTicket =
+                await this.ticketApiBroker.GetTicketByIdAsync(
+                    inputTicket.Id);
+
+            // then
+            actualTicket.Should().BeEquivalentTo(expectedTicket);
+
+            await this.ticketsApiBroker.DeleteTicketByIdAsync(actualTicket.Id);
+        }*/
+    }
+}
