9a14d2cad2cc3e6056d5e6f0c42e80859f26a510
Pwongchaiya <Pwongchaiya1@gmail.com>
2024-09-24 14:41:30 -0700

    add Build Infrastructure

---
+++ TicketMGT.Core.Api.Infrastructure.Build/Program.cs
@@ -0,0 +1,90 @@
+﻿using ADotNet.Clients;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
+using System.Collections.Generic;
+using System.IO;
+
+namespace TicketMGT.Core.Api.Infrastructure.Build
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var aDotNetClient = new ADotNetClient();
+
+            var githubPipeline = new GithubPipeline
+            {
+                Name = "Build & Test Ticket MGT",
+
+                OnEvents = new Events
+                {
+                    Push = new PushEvent
+                    {
+                        Branches = ["main"]
+                    },
+
+                    PullRequest = new PullRequestEvent
+                    {
+                        Branches = ["main"]
+                    }
+                },
+
+                Jobs = new Dictionary<string, Job>
+                {
+                    {
+                      "build",
+                      new Job
+                      {
+                          RunsOn = BuildMachines.Windows2022,
+
+                          Steps =
+                          [
+                              new CheckoutTaskV2
+                              {
+                                  Name = "Check out"
+                              },
+
+                              new SetupDotNetTaskV1
+                              {
+                                  Name = "Setup .Net",
+
+                                  TargetDotNetVersion = new TargetDotNetVersion
+                                  {
+                                      DotNetVersion = "8.0.100",
+                                      IncludePrerelease = true
+                                  }
+                              },
+
+                              new RestoreTask
+                              {
+                                  Name = "Restore"
+                              },
+
+                              new DotNetBuildTask
+                              {
+                                  Name = "Build"
+                              },
+
+                              new TestTask
+                              {
+                                  Name = "Test"
+                              }
+                          ]
+                      }
+                    }
+                }
+            };
+
+            string buildScriptPath = "../../../../.github/workflows/dotnet.yml";
+            string directoryPath = Path.GetDirectoryName(buildScriptPath);
+
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
+            aDotNetClient.SerializeAndWriteToFile(githubPipeline, path: buildScriptPath);
+        }
+    }
+}
