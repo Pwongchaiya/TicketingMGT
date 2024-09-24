4d85d85745d482457926b9d97b2ec948db635605
Pwongchaiya <Pwongchaiya1@gmail.com>
2024-09-24 14:12:35 -0700

    update migrations

---
+++ TicketMGT.Core.Api/Migrations/20240924194741_Ticketsss.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TicketMGT.Core.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Ticketsss : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ParentTicketId",
+                table: "Tickets",
+                type: "uniqueidentifier",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ParentTicketId",
+                table: "Tickets");
+        }
+    }
+}
