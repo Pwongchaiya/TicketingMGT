6ee158de2cd22414cec116b304f20b7c58c145e5
Pwongchaiya <Pwongchaiya1@gmail.com>
2024-09-24 20:41:04 -0700

    ShouldThrowValidationExceptionOnRemoveIfTicketInvalidAndLogItAsync -> PASS
    ShouldThrowValidationExceptionOnRemoveIfTicketDoesNotExistAndLogItAsync -> PASS

--- TicketMGT.Core.Api/Services/Foundations/TicketService.cs
+++ TicketMGT.Core.Api/Services/Foundations/TicketService.cs
@@ -47,14 +47,32 @@ namespace TicketMGT.Core.Api.Services.Foundations
             return maybeTicket;
         });
 
-        public ValueTask<Ticket> RemoveTicketByIdAsync(Guid id)
+        public ValueTask<Ticket> ModifyTicketAsync(Ticket ticket) =>
+        TryCatch(async () =>
         {
-            throw new NotImplementedException();
-        }
+            ValidateTicketOnModify(ticket);
 
-        public ValueTask<Ticket> ModifyTicketAsync(Ticket ticket)
-        {
-            throw new NotImplementedException();
-        }
+            Ticket maybeTicket =
+            await storageBroker.SelectTicketByIdAsync(ticket.Id);
+
+            ValidateStorageTicketOnModify(
+                inputTicket: ticket,
+                storageTicket: maybeTicket);
+
+            return await storageBroker.UpdateTicketAsync(ticket);
+        });
+
+        public ValueTask<Ticket> RemoveTicketByIdAsync(Guid id) =>
+            TryCatch(async () =>
+            {
+                ValidateId(id);
+
+                Ticket maybeTicket = await this.storageBroker
+                    .SelectTicketByIdAsync(id);
+
+                ValidateTicketExists(maybeTicket, id);
+
+                return await this.storageBroker.DeleteTicketAsync(maybeTicket);
+            });
     }
 }
