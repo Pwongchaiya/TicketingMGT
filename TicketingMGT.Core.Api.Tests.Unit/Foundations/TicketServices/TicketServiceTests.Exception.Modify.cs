162cc3ab47df23594fe2a3a91730d7d00bd06070
Pwongchaiya <Pwongchaiya1@gmail.com>
2024-09-24 20:19:22 -0700

    Modify Ticket E2E

---
+++ TicketingMGT.Core.Api.Tests.Unit/Foundations/TicketServices/TicketServiceTests.Exception.Modify.cs
@@ -0,0 +1,216 @@
+﻿using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using TicketMGT.Core.Api.Models.Foundations.Tickets;
+using TicketMGT.Core.Api.Models.Foundations.Tickets.Exceptions;
+
+namespace TicketMGT.Core.Api.Tests.Unit.Foundations.TicketServices
+{
+    public partial class TicketServiceTests
+    {
+        [Fact]
+        private async Task ShouldThrowCriticalDependencyExceptionOnModifyIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            Ticket someTicket = CreateRandomTicket();
+            SqlException sqlException = CreateSqlException();
+
+            var failedTicketStorageException =
+                new FailedTicketStorageException(
+                    message: "Failed ticket storage error occurred, contact support.",
+                    innerException: sqlException);
+
+            var expectedTicketDependencyException =
+                new TicketDependencyException(
+                    message: "Ticket dependency error occurred, contact support.",
+                    innerException: failedTicketStorageException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffset())
+                    .Throws(sqlException);
+
+            // when
+            ValueTask<Ticket> modifyTicketTask =
+                this.ticketService.ModifyTicketAsync(someTicket);
+
+            TicketDependencyException actualTicketDependencyException =
+                await Assert.ThrowsAsync<TicketDependencyException>(
+                    modifyTicketTask.AsTask);
+
+            // then
+            actualTicketDependencyException.Should().BeEquivalentTo(
+                expectedTicketDependencyException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedTicketDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateTicketAsync(someTicket),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        private async Task ShouldThrowDependencyExceptionOnModifyIfDbUpdateConcurrencyErrorOccursAndLogItAsync()
+        {
+            // given
+            var someTicket = CreateRandomTicket();
+            var databaseUpdateConcurrencyException = new DbUpdateConcurrencyException();
+
+            var lockedTicketException =
+                new LockedTicketException(
+                    message: "Locked ticket record exception, please try again later.",
+                    innerException: databaseUpdateConcurrencyException);
+
+            var expectedTicketDependencyValidationException =
+                new TicketDependencyValidationException(
+                    message: "Ticket dependency validation error occurred, fix errors and try again.",
+                    innerException: lockedTicketException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffset())
+                    .Throws(databaseUpdateConcurrencyException);
+
+            // when
+            ValueTask<Ticket> modifyTicketTask =
+                this.ticketService.ModifyTicketAsync(someTicket);
+
+            TicketDependencyValidationException actualTicketDependencyValidationException =
+                await Assert.ThrowsAsync<TicketDependencyValidationException>(
+                    modifyTicketTask.AsTask);
+
+            // then
+            actualTicketDependencyValidationException.Should().BeEquivalentTo(
+                expectedTicketDependencyValidationException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedTicketDependencyValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateTicketAsync(someTicket),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        private async Task ShouldThrowDependencyExceptionOnModifyIfDatabaseUpdateExceptionOccursAndLogItAsync()
+        {
+            // given
+            var someTicket = CreateRandomTicket();
+            var databaseUpdateException = new DbUpdateException();
+
+            var failedTicketStorageException =
+                new FailedTicketStorageException(
+                    message: "Failed ticket storage error occurred, contact support.",
+                    innerException: databaseUpdateException);
+
+            var expectedTicketDependencyException =
+                new TicketDependencyException(
+                    message: "Ticket dependency error occurred, contact support.",
+                    innerException: failedTicketStorageException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffset())
+                    .Throws(databaseUpdateException);
+
+            // when
+            ValueTask<Ticket> modifyTicketTask =
+                this.ticketService.ModifyTicketAsync(someTicket);
+
+            TicketDependencyException actualTicketDependencyException =
+                await Assert.ThrowsAsync<TicketDependencyException>(
+                    modifyTicketTask.AsTask);
+
+            // then
+            actualTicketDependencyException.Should().BeEquivalentTo(
+                expectedTicketDependencyException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedTicketDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateTicketAsync(someTicket),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        private async Task ShouldThrowServiceExceptionOnModifyWhenExceptionOccursAndLogItAsync()
+        {
+            // given
+            var someTicket = CreateRandomTicket();
+            var serviceException = new Exception();
+
+            var failedTicketStorageException =
+                new FailedTicketServiceException(
+                    message: "Failed ticket service error occurred, contact support.",
+                    innerException: serviceException);
+
+            var expectedTicketServiceException =
+                new TicketServiceException(
+                    message: "Ticket service error occurred, contact support.",
+                    innerException: failedTicketStorageException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffset())
+                    .Throws(serviceException);
+
+            // when
+            ValueTask<Ticket> modifyTicketTask =
+                this.ticketService.ModifyTicketAsync(someTicket);
+
+            TicketServiceException actualTicketDependencyException =
+                 await Assert.ThrowsAsync<TicketServiceException>(
+                     modifyTicketTask.AsTask);
+
+            // then
+            actualTicketDependencyException.Should().BeEquivalentTo(
+                expectedTicketServiceException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedTicketServiceException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateTicketAsync(someTicket),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
